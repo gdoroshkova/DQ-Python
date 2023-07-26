947bcb7dd65367feef1b7e2365d974bc35b22ceb
Halina_Darashkova <gjudoroshkova@gmail.com>
2023-07-27 01:22:52 +0200

    add task6

---
+++ task6_module_files.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from OOP_task import UserInteraction
+from function_task3 import case_normalization
+
+class ImportPublications:
+    def __init__(self, file_path="C:\\Users\Halina_Darashkova\PycharmProjects\DQ-Python\\task6_records.txt"):
+        self.file_path = file_path
+        self.records = []
+        self.user_interaction = UserInteraction()
+
+    def download_records(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as file:
+                content = file.read()
+                separator = '**********\n'
+                self.records = content.split(separator)
+
+    def upload_records(self):
+        for record in self.records:
+            if record:
+                rows = record.split('\n')
+                text = case_normalization(rows[1].replace('text: ', ''))
+
+                if record.startswith('News'):
+                    city = rows[2].replace('city: ', '')
+                    self.user_interaction.add_publication("News", text=text, city=city)
+                elif record.startswith('Private Ad'):
+                    expiration_date = rows[2].replace('expiration_date: ', '')
+                    self.user_interaction.add_publication("Advertising", text=text, expiration_date=expiration_date)
+                elif record.startswith('Motivation'):
+                    audience = rows[2].replace('audience: ', '')
+                    self.user_interaction.add_publication("Motivation", text=text, audience=audience)
+        self.user_interaction.save_publication()
+
+    def remove_file(self):
+        os.remove(self.file_path)
+
+
+import_publications = ImportPublications()
+try:
+    import_publications.download_records()
+    import_publications.upload_records()
+    # import_publications.remove_file()
+except:
+    print("Error!")
+
+
+
